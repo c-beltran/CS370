66393cb44197e59be347b25f36cfea1c0b83c4dd
c-beltran <carlosbeltran90@gmail.com>
2018-09-19 17:53:13 -0400

    made progess, able to print .txt file

--- StoreInventory/src/storeInventory.java
+++ StoreInventory/src/storeInventory.java
@@ -1,5 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 /**
  * @author Carlos Alberto
  *This 
@@ -8,39 +13,47 @@ public class storeInventory {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		HashMap<Integer, HashMap<String, String>> carsMap = new HashMap<Integer, HashMap<String, String>>();
 		//shows map is empty
 //		print(carsMap);
 		
 		//inserting datato MAINHASH
 		carsMap.put(1, new HashMap(){{put("name", "Toyota");}});
+		carsMap.get(1).put("price", "10000");
+		carsMap.get(1).put("orderNum", "5");
+		carsMap.get(1).put("quantity", "11");		
+		
 		carsMap.put(2, new HashMap(){{put("name", "Nissan");}});
 		
-
-		System.out.println(carsMap.get(1).get("name"));
+		String name = carsMap.get(1).get("name");
+		String price = carsMap.get(1).get("price");
+		String orderNum = carsMap.get(1).get("orderNum");
+		String quantity = carsMap.get(1).get("quantity");
 		
+		System.out.println("this is the name " + name );
+		
+		//Print!!!!
+		print(name, price, orderNum, quantity);
+
+		System.out.println("Car 1 name is: " + carsMap.get(1).get("name"));
+		System.out.println("Car 2 name is: " + carsMap.get(2).get("name"));
 		
 		//print size of map
 		System.out.println("size of map is: " + carsMap.size());
 		
-		//print all values of hashmap
-//		for(int i=0; i< carsMap.size(); i++){
-//			if(carsMap.containsKey(i)){
-//				System.out.println(carsMap.get(i));
-//			}else{
-//				System.out.println("ERROR");
-//			}
-//		}
+		
 	}
 	
-//	public static void print(Map<Integer, Object> map){
-//		if(map.isEmpty()){
-//			System.out.println("Cars Map is Empty");
-//		}else{
-//			System.out.println(map);
-//		}
-//	}
+	public static void print(String n, String p, String o, String q) throws IOException{
+		File fout = new File("Output//Result"+".txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		bw.write("Name \t" + "Price \t" + "OrderNum \t" + "Quantity \n");
+		bw.write(n + " \t" + p + " \t" + o + " \t" + q);
+		bw.close();
+	}
 
 }
