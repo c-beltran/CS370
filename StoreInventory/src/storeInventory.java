357701103a87602cb4002c5faff03f2e1982b28b
c-beltran <carlosbeltran90@gmail.com>
2018-09-19 16:14:51 -0400

    simple hashmap

--- StoreInventory/src/storeInventory.java
+++ StoreInventory/src/storeInventory.java
@@ -1,4 +1,5 @@
-
+import java.util.HashMap;
+import java.util.Map;
 /**
  * @author Carlos Alberto
  *This 
@@ -9,8 +10,34 @@ public class storeInventory {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello World");
-
+		HashMap<Integer, String> carsMap = new HashMap<>();
+		//shows map is empty
+		print(carsMap);
+		String[] arr = {"Toyota", "Honda", "Nissan", "Hyundai", "Mercedes", "BMW"};
+		
+		for(int i=0; i< arr.length; i++){
+			carsMap.put(i, arr[i]);
+		}
+		
+		//print size of map
+		System.out.println("size of map is: " + carsMap.size());
+		
+		//print all values of hashmap
+		for(int i=0; i< carsMap.size(); i++){
+			if(carsMap.containsKey(i)){
+				System.out.println(carsMap.get(i));
+			}else{
+				System.out.println("ERROR");
+			}
+		}
+	}
+	
+	public static void print(Map<Integer, String> map){
+		if(map.isEmpty()){
+			System.out.println("Cars Map is Empty");
+		}else{
+			System.out.println(map);
+		}
 	}
 
 }
