e08fd2094dc2e68a272457d202968fb3b5985376
c-beltran <carlosbeltran90@gmail.com>
2018-09-19 14:41:46 -0400

    env set up

---
+++ StoreInventory/src/storeInventory.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author Carlos Alberto
+ *This 
+ */
+public class storeInventory {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+	}
+
+}
